95d7e9f70e75ba985e8308dd9cb45339f6b4d331
shashank42 <devo.shanky@gmail.com>
2023-03-05 04:04:53 -0700

    Update to matic for opensea ss

--- contract.py
+++ contract.py
@@ -5,12 +5,15 @@ from web3.providers.rpc import HTTPProvider
 from web3.middleware import geth_poa_middleware
 import asyncio
 
+# Matic
+contractAddress = '0xe9AbD1263b5D024d6FC53fa1F98A4560615761B0'
 
+# Coinbase
 contractAddress = '0x9d5CD448332A857F6BfDb7541CFc33C61789BB41'
 address2 = Web3.toChecksumAddress(contractAddress)
 
-# PROVIDER = "wss://polygon-mainnet.g.alchemy.com/v2/_tn9X7pFnXwYXYi8Q33gQjRg_B3Dey_4"
-PROVIDER = "https://goerli.base.org"
+PROVIDER = "wss://polygon-mainnet.g.alchemy.com/v2/_tn9X7pFnXwYXYi8Q33gQjRg_B3Dey_4"
+# PROVIDER = "https://goerli.base.org"
 # web3 = Web3(Web3.WebsocketProvider(PROVIDER))
 web3 = Web3(Web3.HTTPProvider(PROVIDER))
 web3.middleware_onion.inject(geth_poa_middleware, layer=0)
